aeb1a114d8784b532d1f871417f423f60d0a74fa
Igor Frey <igorfreyy97@gmail.com>
2021-07-24 23:10:07 -0300

    Criação do serviço para retornar video específico

--- src/main/java/br/com/backend/alurachallenge/controllers/VideoController.java
+++ src/main/java/br/com/backend/alurachallenge/controllers/VideoController.java
@@ -1,11 +1,15 @@
 package br.com.backend.alurachallenge.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +25,25 @@ public class VideoController {
 	private VideoRepository videoRepository;
 	
 	@GetMapping
-	public Page<VideoDto> lista(
+	public Page<VideoDto> listaVideos (
 			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 
 		Page<Video> videos = videoRepository.findAll(paginacao);
 		return VideoDto.converter(videos);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<VideoDto> retornaVideo (@PathVariable Long id) {
+
+		
+		Optional<Video> video = videoRepository.findById(id);
+		
+		if(video.isPresent()) {
+			
+			return ResponseEntity.ok(new VideoDto(video.get()));
+		} 
+		
+		
+		return ResponseEntity.notFound().build();
 	}
 }
